97e22f87a5cea67d3ec4f2e4e1b193ca3bbdf2cf
Margret Riegert <margret@eowyn.net>
2025-01-19 21:43:04 -0500

    Load languages from language paths instead of cache

--- src/tree_sitter/repository.cr
+++ src/tree_sitter/repository.cr
@@ -19,43 +19,49 @@ module TreeSitter
     end
 
     def self.preload_all
-      language_paths.each_key { |name| load_language?(name) }
+      language_paths.each { |name, path| load_language?(name, path) }
     end
 
     def self.language_names : Array(String)
       language_paths.keys
     end
 
-    def self.load_language(name : String) : Language
+    def self.load_language(name : String)
+      load_language(name, language_paths[name])
+    end
+
+    def self.load_language(name : String, path : Path) : Language
       lang_path = language_paths[name]?
       raise Error.new("Unknown language: #{name}.") if lang_path.nil?
 
-      ts_lang = load_shared_object(name)
+      ts_lang = load_shared_object(name, path)
       Language.new(name, ts_lang)
     end
 
-    def self.load_language?(name : String) : Language?
-      load_language(name)
+    def self.load_language?(name : String, path : Path) : Language?
+      load_language(name, path)
     rescue Error
       nil
     end
 
-    def self.load_shared_object(langguage_name : String) : LibTreeSitter::TSLanguage
-      cache_dir = ENV["XDG_CACHE_HOME"]? || Path.home.join(".cache")
-      cache_dir = Path.new(cache_dir)
-      so_path = cache_dir.join("tree-sitter", "lib", "#{langguage_name}.so")
+    def self.load_shared_object(language_name : String, language_path : Path) : LibTreeSitter::TSLanguage
+      file_extension = {% if flag?(:darwin) %}
+                         "dylib"
+                       {% else %}
+                         "so"
+                       {% end %}
+
+      so_path = language_path.join("libtree-sitter-#{language_name}.#{file_extension}")
 
       raise Error.new("#{so_path} doesn't exists. Create it using tree-sitter CLI.") unless File.exists?(so_path)
 
       handle = LibC.dlopen(so_path.to_s, LibC::RTLD_LAZY | LibC::RTLD_LOCAL)
-      raise Error.new("Can't load language #{langguage_name}. #{so_path} was not found.") if handle.null?
+      raise Error.new("Can't load language #{language_name}. #{so_path} was not found.") if handle.null?
 
-      ptr = LibC.dlsym(handle, "tree_sitter_#{langguage_name}")
-      raise Error.new("Can't find symbol tree_sitter_#{langguage_name} at #{so_path}.") unless ptr
+      ptr = LibC.dlsym(handle, "tree_sitter_#{language_name}")
+      raise Error.new("Can't find symbol tree_sitter_#{language_name} at #{so_path}.") unless ptr
 
       Proc(LibTreeSitter::TSLanguage).new(ptr, Pointer(Void).null).call
     end
   end
 end
-
-TreeSitter::Repository.load_language?("ruby")
